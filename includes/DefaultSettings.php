6364f6945aad9eee14ebe1ff756ce05a0f2f8b0e
Robert Vogel <vogel@hallowelt.biz>
2016-12-13 11:10:00 +0100

    Fixing for REL1_27
    As https://gerrit.wikimedia.org/r/#/c/326127/ was a dead end, this is the
    new approach to fix the extension for REL1_27
    
    Change-Id: I38843ba186bbb3e620c0b2e7e252dbdf9827f477

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5,14 +5,8 @@
  */
 
 // Remove the default illegal char ':' - needed it to determine NS
-$GLOBALS['wgIllegalFileChars'] = str_replace( ":","",$GLOBALS['wgIllegalFileChars'] );
+$GLOBALS['wgIllegalFileChars'] = str_replace( ":", "", $GLOBALS['wgIllegalFileChars'] );
 
-//Activate img_auth.php
-$GLOBALS['wgUploadPath'] = $GLOBALS['wgScriptPath'] .'/img_auth.php';
-$GLOBALS['wgImgAuthUrlPathMap']['/nsfilerepo/'] = 'mwstore://nsfilerepo-fs/namespace/';
-$GLOBALS['wgFileBackends'][] = array(
-	'name'        => 'nsfilerepo-fs',
-	'class'       => 'NSFileRepoFSFileBackend',
-	'lockManager' => 'fsLockManager',
-	#'wikiId'       => wfWikiID()
-);
+//Activate "nsfr_img_auth.php"
+//This may be obsolete in future MW versions
+$GLOBALS['wgUploadPath'] = $GLOBALS['wgScriptPath'] .'/nsfr_img_auth.php';
