d441a5e9c5edb40bd09d8f4906bb11f2e5584c95
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-04-25 13:16:25 +0200

    Fix for missing arhive images
    publishTo function replaces name with NS prefix with stripped name.
    Problem is that this function returns name for both physical file and
    name in DB (oldimage table, oi_archive_name).
    Name for FS should be stripped but the one for DB should not
    
    Change-Id: I8fdc86ccd0e6478fb2bb2a02ac15f66f406c7ebb
    ERM: #4275

--- NSFileRepo_body.php
+++ NSFileRepo_body.php
@@ -283,10 +283,11 @@ class NSLocalFile extends LocalFile {
 
 		$this->lock(); // begin
 
+		$archiveName = wfTimestamp( TS_MW ) . '!'. $this->getName();
 /* This is the part that changed from LocalFile */
-		$archiveName = wfTimestamp( TS_MW ) . '!'. $this->getFileNameStripped( $this->getName() );
+		$strippedArchiveName = wfTimestamp( TS_MW ) . '!'. $this->getFileNameStripped( $this->getName() );
+		$archiveRel = 'archive/' . $this->getHashPath() . $strippedArchiveName;
 /* End of changes */
-		$archiveRel = 'archive/' . $this->getHashPath() . $archiveName;
 		$flags = $flags & File::DELETE_SOURCE ? LocalRepo::DELETE_SOURCE : 0;
 		$status = $this->repo->publish( $srcPath, $dstRel, $archiveRel, $flags, $options );
 
