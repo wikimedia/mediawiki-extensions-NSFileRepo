9ebc3d89c00b1013e5d8ebccd809eb8fa6dedbae
rvogel <vogel@hallowelt.biz>
2017-07-11 16:29:59 +0200

    Adding PSR-4 Autoloader and new NamespaceList
    Change-Id: I1931e2b355cacabbcdbe3af29d3e9a513e4a316f
    (cherry picked from commit 4a124d8f2b4ffeea898479cfc39e0f586e21411a)

---
+++ src/MWNamespace.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace NSFileRepo;
+
+class MWNamespace implements \JsonSerializable {
+
+	/**
+	 *
+	 * @var int
+	 */
+	protected $id = '';
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $canonicalName = '';
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $displayName = '';
+
+	/**
+	 *
+	 * @param int $id
+	 * @param string $canonicalName
+	 * @param string $displayName
+	 */
+	public function __construct( $id, $canonicalName, $displayName ) {
+		$this->id = (int)$id;
+		$this->canonicalName = $canonicalName;
+		$this->displayName = $displayName;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getCanonicalName() {
+		return $this->canonicalName;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getDisplayName() {
+		return $this->displayName;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function jsonSerialize() {
+		return [
+			'id' => $this->getId(),
+			'canonicalName' => $this->getCanonicalName(),
+			'displayName' => $this->getDisplayName()
+		];
+	}
+
+}
---
+++ tests/phpunit/NamespaceListTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace NSFileRepo\Tests;
+
+class NamespaceListTest extends \MediaWikiLangTestCase {
+
+	const DUMMY_NS_A_ID = 12412;
+	const DUMMY_NS_B_ID = 12512;
+	const DUMMY_NS_C_ID = 12612;
+
+	protected function setUp() {
+		global $wgExtraNamespaces, $wgNamespaceContentModels, $wgContentHandlers, $wgContLang;
+
+		parent::setUp();
+
+		$this->setMwGlobals( [
+			'wgExtraNamespaces' => $wgExtraNamespaces,
+			'wgNamespaceContentModels' => $wgNamespaceContentModels,
+			'wgContentHandlers' => $wgContentHandlers,
+			'wgContLang' => $wgContLang,
+		] );
+
+		$wgExtraNamespaces[self::DUMMY_NS_A_ID] = 'NSFRDummyA';
+		$wgExtraNamespaces[self::DUMMY_NS_A_ID + 1] = 'NSFRDummyA_talk';
+
+		$wgExtraNamespaces[self::DUMMY_NS_B_ID] = 'NSFRDummyB';
+		$wgExtraNamespaces[self::DUMMY_NS_B_ID + 1] = 'NSFRDummyB_talk';
+
+		$wgExtraNamespaces[self::DUMMY_NS_C_ID] = 'NSFRDummyC';
+		$wgExtraNamespaces[self::DUMMY_NS_C_ID + 1] = 'NSFRDummyC_talk';
+
+		\MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
+		$wgContLang->resetNamespaces(); # reset namespace cache
+
+		/**
+		 * Test hook handler that mimics Extension:Lockdown and revokes read
+		 * permissions on 'NSFRDummyA' and edit permission on 'NSFRDummyB'
+		 */
+		\Hooks::register( 'userCan', function( &$title, &$user, $action, &$result ) {
+			if( $action === 'read'
+					&& $title instanceof \Title
+					&& $title->getNamespace() === self::DUMMY_NS_A_ID ) {
+				return false;
+			}
+
+			if( $action === 'edit'
+					&& $title instanceof \Title
+					&& $title->getNamespace() === self::DUMMY_NS_B_ID ) {
+				return false;
+			}
+
+			return true;
+		} );
+	}
+
+	public function textInstance() {
+		$namespacelist = $this->makeInstance();
+		$this->assertInstanceOf( 'NSFileRepo\NamespaceList', $namespacelist );
+	}
+
+	public function testGetReadableNoTalks() {
+		$instance = $this->makeInstance( new \HashConfig([
+			\NSFileRepo\Config::CONFIG_SKIP_TALK => true
+		]) );
+
+		$readables = $instance->getReadable();
+		$hasTalk = false;
+		foreach( $readables as $namsepace ) {
+			if( \MWNamespace::isTalk( $namsepace->getId() ) ) {
+				$hasTalk = true;
+				break;
+			}
+		}
+
+		$this->assertFalse( $hasTalk, 'List should not contain any Talk namespaces' );
+	}
+
+	public function testGetReadableNoUnreadables() {
+		$instance = $this->makeInstance( new \HashConfig([
+			\NSFileRepo\Config::CONFIG_SKIP_TALK => true
+		]) );
+
+		$readables = $instance->getReadable();
+		$hasUnreadables = false;
+		foreach( $readables as $namsepace ) {
+			if( $namsepace->getId() === self::DUMMY_NS_A_ID ) {
+				$hasUnreadables = true;
+				break;
+			}
+		}
+
+		$this->assertFalse( $hasUnreadables, 'List should not contain an unreadable namespace' );
+	}
+
+	protected function makeInstance( $config = null ) {
+		if( $config === null ) {
+			$config = new \HashConfig( [] );
+		}
+
+		$user = \RequestContext::getMain()->getUser();
+		$lang = \RequestContext::getMain()->getLanguage();
+
+		return new \NSFileRepo\NamespaceList( $user, $config, $lang );
+	}
+
+}
