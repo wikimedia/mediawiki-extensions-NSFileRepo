616e8d15682ff732162928213d67819df6ea1bf2
miriam schlindwein <schlindwein@hallowelt.com>
2022-05-11 10:41:35 +0200

    Add prefixes to upload params
    ERM27833
    
    [4.x]
    
    Change-Id: I5f0f175846bb1337ea04e25c7de447a12deba27f

--- resources/ext.nsfilerepo.namespace.upload.js
+++ resources/ext.nsfilerepo.namespace.upload.js
@@ -1,7 +1,8 @@
 ( function ( mw ) {
 
 	var targetNamespaceSelector;
-	mw.hook( 'upload.init' ).add( function ( $container ) {
+	mw.hook( 'upload.init' ).add( function ( $container, value ) {
+		value = value || '';
 		targetNamespaceSelector = new mw.widgets.NamespaceInputWidget( {
 			exclude: '',
 			dropdown: {
@@ -17,17 +18,74 @@
 		$container.$element.prepend( namespaceInputField.$element );
 	} );
 
-	mw.hook( 'upload.getUploadParams' ).add( function ( params ) {
+	mw.hook( 'upload.getUploadParams' ).add( function ( params, defaultPrefix ) {
 		var namespaces = mw.config.get( 'wgFormattedNamespaces' );
 		var selectedNamespace = targetNamespaceSelector.getValue();
+		defaultPrefix = defaultPrefix || '';
+		if ( defaultPrefix.length > 0 ) {
+			defaultPrefix = defaultPrefix.split( ':' );
+			if ( defaultPrefix.length > 1 ) {
+				defaultPrefix = defaultPrefix[ 1 ] + '.';
+			} else {
+				defaultPrefix = defaultPrefix[ 0 ] + '.';
+			}
+		}
 
 		var prefix = '';
 		if ( selectedNamespace !== '0' ) { // NS_MAIN
 			prefix = namespaces[ selectedNamespace ] + ':';
 		}
 		var name = params.filename;
-		var newName = prefix + name;
+		var newName = prefix + defaultPrefix + name;
 		params.filename = newName;
+	} );
+
+	mw.hook( 'upload.setNamespaceValue' ).add( function ( prefix ) {
+		var namespace = prefix.split( ':' );
+		var namespaces = mw.config.get( 'wgFormattedNamespaces' );
+		for ( var namespaceId in namespaces ) {
+			if ( namespaces[ namespaceId ] === namespace[ 0 ] ) {
+				targetNamespaceSelector.setValue( namespaceId );
+			}
+		}
+	} );
+
+	mw.hook( 'upload.getPrefixParams' ).add( function ( params, defaultPrefix ) {
+		var namespaces = mw.config.get( 'wgFormattedNamespaces' );
+		var namespace = '';
+		defaultPrefix = defaultPrefix || '';
+		if ( defaultPrefix.length > 0 ) {
+			defaultPrefix = defaultPrefix.split( ':' );
+
+			namespace = defaultPrefix[ 0 ];
+			if ( defaultPrefix.length > 1 ) {
+				defaultPrefix = defaultPrefix[ 1 ] + '.';
+			}
+		}
+
+		prefix = '';
+		namespaceValue = false;
+		for ( var namespaceId in namespaces ) {
+			if ( namespaces[ namespaceId ] === namespace ) {
+				if ( namespaceId !== '0' ) {
+					prefix = namespaces[ namespaceId ] + ':';
+				}
+				namespaceValue = true;
+			}
+		}
+		var name = params.filename;
+		var newName = '';
+		if ( namespaceValue ) {
+			newName = prefix + defaultPrefix + name;
+			params.filename = newName;
+		} else {
+			if ( defaultPrefix.length > 1 ) {
+				newName = namespace + ':' + defaultPrefix + name;
+			} else {
+				newName = defaultPrefix + '.' + name;
+			}
+			params.filename = newName;
+		}
 	} );
 
 }( mediaWiki ) );
