4d26afae7b49cc7ce476669f2d1e790ddb63db1b
akulbii <alk@astarios.com>
2023-07-13 09:42:06 +0300

    Consider custom namespaces when creating Drawio and BPMN diagrams
    [4.3.x]
    
    ERM33034
    
    Change-Id: I4974fd60d115fbd144da09a9a11c8a9c32cfb233

---
+++ resources/nsfr/NamespaceFilenameProcessor.js
@@ -0,0 +1,39 @@
+window.nsfr = window.nsfr || {};
+
+nsfr.NamespaceFilenameProcessor = function () {};
+
+/**
+ * @inheritDoc
+ */
+nsfr.NamespaceFilenameProcessor.prototype.initializeFilename = function () {
+	var filename = mw.config.get( 'wgPageName' ) + '-' + ( Math.floor( Math.random() * 100000000 ) + 1 );
+	// filename must only contain alphanumeric characters, colons (for namespaces),
+	// dashes and underscores
+	filename = this.sanitizeFilename( filename );
+
+	return filename;
+};
+
+/**
+ * @inheritDoc
+ */
+nsfr.NamespaceFilenameProcessor.prototype.validateFilename = function ( filename ) {
+	if ( filename === '' ) {
+		return false;
+	}
+	if ( !filename.match( /^[\w,-.:\s]+$/ ) ) {
+		return false;
+	}
+	return true;
+};
+
+/**
+ * @inheritDoc
+ */
+nsfr.NamespaceFilenameProcessor.prototype.sanitizeFilename = function ( filename ) {
+	// filename must only contain alphanumeric characters, colons (for namespaces),
+	// dashes and underscores
+	filename = filename.replace( /[^a-zA-Z0-9_:-]/g, '_' );
+
+	return filename;
+};
