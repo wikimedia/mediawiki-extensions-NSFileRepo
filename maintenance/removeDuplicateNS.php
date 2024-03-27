215e9081b7e8c0f953489ea9abdc3e020f971d45
hslater <hamish.slater@hallowelt.com>
2024-03-27 17:07:04 +1300

    Replace deprecated wfGetDB
    ERM36615
    
    [REL1_39, master]
    
    Change-Id: I590b67455a8ec13e73a23379259a8d795ec0a16a

--- maintenance/removeDuplicateNS.php
+++ maintenance/removeDuplicateNS.php
@@ -15,7 +15,7 @@ class RemoveDuplicateNS extends Maintenance {
 	}
 
 	function execute() {
-		$dbw = wfGetDB( DB_PRIMARY );
+		$dbw = $this->getDB( DB_PRIMARY );
 		print( "USING DB: " . $dbw->getDBName() . "\n" );
 		$images = $dbw->select( 'oldimage',
 			array( 'oi_name', 'oi_archive_name' ),
