b2707ecbf42ef0cb9672c12c8a0cbeef2b87da69
d.savuljesku <d.savuljesku@gmail.com>
2024-06-07 13:21:57 +0200

    Allow for proper namespace filtering of Special:Filelist
    [4.5.x]
    
    ERM33558
    
    Change-Id: Iff69b0e68bf398316c654b00dbc40f3a0c9eb760

--- resources/ext.nsfilerepo.filelist.js
+++ resources/ext.nsfilerepo.filelist.js
@@ -1,10 +1,15 @@
 mw.hook( 'enhanced.filelist.gridconfig' ).add( function ( columnCfg ) {
+	var nsBuckets = mw.config.get( 'bsgNSFileRepoAvailableNamespaces' ) || [];
 	// eslint-disable-next-line camelcase
 	columnCfg.namespace_text = {
 		headerText: mw.message( 'nsfilerepo-enhanced-filelist-grid-namespace-title' ).text(),
 		type: 'text',
 		sortable: true,
-		filter: { type: 'string' },
+		filter: {
+			type: 'list',
+			list: nsBuckets,
+			closePopupOnChange: true
+		},
 		hidden: !mw.user.options.get( 'filelist-show-namespace_text' )
 	};
 } );
