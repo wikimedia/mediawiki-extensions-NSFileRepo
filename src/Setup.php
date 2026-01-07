67c48218a9a3e269525166243055deae26eee64c
Robert Vogel <vogel@hallowelt.biz>
2026-01-07 15:44:11 +0100

    Fix implicit config issue in context of update.php
    ERM45773
    
    [5.1.x]
    
    Reverts https://gerrit.wikimedia.org/r/c/mediawiki/extensions/NSFileRepo/+/1220319/1/src/Setup.php#b13
    
    Change-Id: Idd54041811071c788c95727713bd9832259b2dc5

--- src/Setup.php
+++ src/Setup.php
@@ -9,7 +9,12 @@ class Setup {
 	 */
 	public static function register() {
 		$GLOBALS['wgLocalFileRepo']['class'] = NamespaceLocalRepo::class;
-		if ( defined( 'MW_QUIBBLE_CI' ) ) {
+		if ( PHP_SAPI === 'cli' ) {
+			// Avoid `--skip-config-validation` issues in context of `update.php`
+			// Unfortunately there is no good way to know if in context of this particular maintenance script
+			// Therefore we need to bail out in all CLI context.
+			// HINT: In MW 1.43 `$wgIllegalFileChars` can still be altered, even though it is deprecated since 1.41
+			// Future major releases of this extension need to solve this.
 			return;
 		}
 		// Remove the default illegal char ':' - needed it to determine NS
