93c070c34c8658bca82009abf9f28a70d2c1016a
My Name <jonas.hillebrand@hallowelt.com>
2026-05-27 10:34:31 +0200

    Align NSFileRepo dialog design with standard media dialog
    ERM46059
    
    [5.1.x]
    
    Change-Id: If03101db85ee3469704fbf426559519dba26c482

--- resources/ext.nsfilerepo.special.upload.js
+++ resources/ext.nsfilerepo.special.upload.js
@@ -17,11 +17,24 @@
 
 	function patchDialog() {
 		const DialogProto = mw.Upload.Dialog.prototype;
+		const DialogStatic = mw.Upload.Dialog.static;
 
 		if ( DialogProto.nsfrPatched ) {
 			return;
 		}
 		DialogProto.nsfrPatched = true;
+
+		// Match VisualEditor media dialog behavior: render the info-step back
+		if ( Array.isArray( DialogStatic.actions ) ) {
+			DialogStatic.actions = DialogStatic.actions.map( ( action ) => {
+				if ( action.action === 'cancelupload' ) {
+					return Object.assign( {}, action, {
+						flags: [ 'safe', 'back' ]
+					} );
+				}
+				return action;
+			} );
+		}
 
 		const originalOnSet = DialogProto.onUploadBookletSet;
 
@@ -45,22 +58,28 @@
 			return;
 		}
 
-		const $nsDropdown = $( '<select>' ).css( { width: '100%' } );
-		for ( const [ label, value ] of Object.entries( namespaces ) ) {
-			$nsDropdown.append(
-				$( '<option>' ).attr( 'value', value ).text( label )
-			);
+		const namespaceOptions = Object.entries( namespaces ).map( ( [ label, value ] ) => ( {
+			data: value,
+			label: label
+		} ) );
+
+		const namespaceSelector = new OO.ui.DropdownInputWidget( {
+			options: namespaceOptions
+		} );
+		const namespaceSelectorLayout = new OO.ui.FieldLayout( namespaceSelector, {
+			label: mw.message( 'namespace' ).text(),
+			align: 'top'
+		} );
+		namespaceSelectorLayout.$element.addClass( 'nsfr-namespace-selector' );
+
+		const $fieldsetGroup = $fieldset.find( '.oo-ui-fieldsetLayout-group' ).first();
+		if ( $fieldsetGroup.length ) {
+			// Keep the Details legend/title first, then insert namespace as first
+			// field inside the fieldset content.
+			$fieldsetGroup.prepend( namespaceSelectorLayout.$element );
+		} else {
+			$fieldset.append( namespaceSelectorLayout.$element );
 		}
-
-		const $wrapper = $( '<div>' )
-			.addClass( 'nsfr-namespace-selector' )
-			.css( { marginBottom: '1em' } )
-			.append(
-				$( '<label>' ).text( mw.message( 'namespace' ).text() ),
-				$nsDropdown
-			);
-
-		$fieldset.prepend( $wrapper );
 
 		const $filenameInput = page.$element
 			.find( '.oo-ui-textInputWidget input' )
@@ -76,7 +95,7 @@
 
 		// Update filename to include selected namespace
 		function updateFilename() {
-			const nsPrefix = $nsDropdown.val();
+			const nsPrefix = namespaceSelector.getValue();
 			const current = $filenameInput.val() || '';
 			const baseName = stripNamespace( current );
 
@@ -88,7 +107,7 @@
 		}
 
 		// Event: namespace selection change → rewrite filename
-		$nsDropdown.on( 'change', updateFilename );
+		namespaceSelector.on( 'change', updateFilename );
 
 		// Event: typing in filename → preserve namespace prefix
 		$filenameInput.on( 'input', () => {
